f1d9a3a62641b0ed3f42d4bc17f495cfaf6b3cce
Botosel04 <filip.pintea@gmail.com>
2023-12-07 11:31:28 +0200

    update1

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -32,7 +32,7 @@ export const Sidenav = () => {
           <a
             onClick={handleNav}
             href="#work"
-            className="mt-5s w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100  shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
+            className="mt-5  w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100  shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
             <span className="pl-4">Work</span>
